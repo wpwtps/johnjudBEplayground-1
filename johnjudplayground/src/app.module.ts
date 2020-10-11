dbf000d7930a1a7c2968b8c7b802e58cf4b27cfb
MasterPoon <chirapat.ba@ku.th>
2020-10-11 10:56:57 +0700

    app - implement typeorm & graphql

--- johnjudplayground/src/app.module.ts
+++ johnjudplayground/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/johnjud',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        User,
+      ]
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
