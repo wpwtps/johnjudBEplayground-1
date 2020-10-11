ef889959da5f57de79290e8ca75f6f1bf29f8b7e
MasterPoon <chirapat.ba@ku.th>
2020-10-11 10:53:06 +0700

    user - init

---
+++ johnjudplayground/src/user/user.entity.ts
@@ -0,0 +1,54 @@
+import { Double } from "mongodb";
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @ObjectIdColumn()
+    _id: string;
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    UserName: string;
+
+    @Column()
+    Password: string;
+
+    @Column()
+    FirstName: string;
+
+    @Column()
+    LastName: string;
+
+    @Column()
+    ProfilePicURL: string;
+
+    @Column()
+    Birthday: Date;
+
+    @Column()
+    Gender: string;
+
+    @Column()
+    PhoneNo: string;
+
+    @Column()
+    Email: string;
+
+    @Column()
+    LocationLat: string;
+
+    @Column()
+    LocationLong: string;
+
+    @Column()
+    AvgPoint: Double;
+
+    @Column()
+    Description: string;
+
+    @Column()
+    TimeUpdate: Date;
+
+}
