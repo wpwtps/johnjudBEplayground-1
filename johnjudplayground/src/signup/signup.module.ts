3bcf14cd47218bc4e175e51dbd32b16c572a2f6c
MasterPoon <chirapat.ba@ku.th>
2020-10-14 15:46:53 +0700

    /signup/verify-email - init

--- johnjudplayground/src/signup/signup.module.ts
+++ johnjudplayground/src/signup/signup.module.ts
@@ -4,10 +4,11 @@ import { SignupController } from './signup.controller';
 import { CheckEmailModule } from './check-email/check-email.module';
 import { LogininfoModule } from './logininfo/logininfo.module';
 import { UserInfoModule } from './user-info/user-info.module';
+import { VerifyEmailModule } from './verify-email/verify-email.module';
 
 @Module({
   providers: [SignupService],
   controllers: [SignupController],
-  imports: [CheckEmailModule, LogininfoModule, UserInfoModule]
+  imports: [CheckEmailModule, LogininfoModule, UserInfoModule, VerifyEmailModule]
 })
 export class SignupModule {}
