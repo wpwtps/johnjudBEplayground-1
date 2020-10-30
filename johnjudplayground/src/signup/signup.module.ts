fbc4240140c79aebf59a21ce57d8e9622b5f1029
MasterPoon <chirapat.ba@ku.th>
2020-10-30 18:06:43 +0700

    keep old signup as V1

--- johnjudplayground/src/signup/signup.module.ts
+++ johnjudplayground/src/signup/signup.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignupController } from './signup.controller';
-import { CheckEmailModule } from './check-email/check-email.module';
-import { LogininfoModule } from './logininfo/logininfo.module';
-import { UserInfoModule } from './user-info/user-info.module';
-import { VerifyEmailModule } from './verify-email/verify-email.module';
-import { VerifyPhoneModule } from './verify-phone/verify-phone.module';
+import { CheckEmailModule } from './v1/check-email/check-email.module';
+import { LogininfoModule } from './v1/logininfo/logininfo.module';
+import { UserInfoModule } from './v1/user-info/user-info.module';
+import { VerifyEmailModule } from './v1/verify-email/verify-email.module';
+import { VerifyPhoneModule } from './v1/verify-phone/verify-phone.module';
+import { V1Module } from './v1/v1.module';
 
 @Module({
   providers: [SignupService],
   controllers: [SignupController],
-  imports: [CheckEmailModule, LogininfoModule, UserInfoModule, VerifyEmailModule, VerifyPhoneModule]
+  imports: [CheckEmailModule, LogininfoModule, UserInfoModule, VerifyEmailModule, VerifyPhoneModule, V1Module]
 })
 export class SignupModule {}
--- johnjudplayground/src/user/edit-user/change-password/change-password.module.ts
+++ johnjudplayground/src/user/edit-user/change-password/change-password.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
-import { LogininfoModule } from 'src/signup/logininfo/logininfo.module';
+import { LogininfoModule } from 'src/signup/v1/logininfo/logininfo.module';
 import { User } from 'src/user/user.entity';
 import { ChangePasswordController } from './change-password.controller';
 import { ChangePasswordService } from './change-password.service';
