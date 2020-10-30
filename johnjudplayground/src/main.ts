f7b183685a99782f3a2eb8dc039ddc6f5e75830e
MasterPoon <chirapat.ba@ku.th>
2020-10-30 18:54:24 +0700

    update policy

--- johnjudplayground/src/main.ts
+++ johnjudplayground/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ 
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
+  await app.listen(2000);
 }
 bootstrap();
