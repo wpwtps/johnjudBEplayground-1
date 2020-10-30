860640d6c82156e839e6bd8c46b32324a75f4ec6
MasterPoon <chirapat.ba@ku.th>
2020-10-30 18:52:21 +0700

    add feature register

---
+++ johnjudplayground/src/signup/v2/register/register.input.ts
@@ -0,0 +1,55 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsDate, IsDateString, IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString, IsUrl, Length, Matches, MaxLength, MinLength } from "class-validator";
+
+@InputType()
+export class RegisterInput{
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Field()
+    UserName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        { message: 'password too weak' },
+    )
+    @Field()
+    Password: string;
+
+    @IsNotEmpty()
+    @Field()
+    FirstName: string;
+
+    @IsNotEmpty()
+    @Field()
+    LastName: string;
+
+    @IsNotEmpty()
+    @IsDateString()
+    @Field()
+    Birthday: Date;
+
+    @IsNotEmpty()
+    @Field()
+    Gender: string;
+
+    @IsNotEmpty()
+    @IsPhoneNumber("TH")
+    @Field()
+    PhoneNo: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    @Field()
+    Email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Field()
+    Address: string;
+}
--- johnjudplayground/src/signup/v2/v2.module.ts
+++ johnjudplayground/src/signup/v2/v2.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { V2Controller } from './v2.controller';
 import { V2Service } from './v2.service';
+import { RegisterModule } from './register/register.module';
 
 @Module({
   controllers: [V2Controller],
-  providers: [V2Service]
+  providers: [V2Service],
+  imports: [RegisterModule]
 })
 export class V2Module {}
