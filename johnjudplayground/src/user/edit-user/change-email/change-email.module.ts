124d3c000105a26221e9b9da1e6b8c8a843e784e
MasterPoon <chirapat.ba@ku.th>
2020-10-31 14:55:00 +0700

    add feature change email

--- johnjudplayground/src/user/edit-user/change-email/change-email.module.ts
+++ johnjudplayground/src/user/edit-user/change-email/change-email.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChangeEmailService } from './change-email.service';
 import { ChangeEmailController } from './change-email.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports:[
+    AuthModule,
+  ],
   providers: [ChangeEmailService],
   controllers: [ChangeEmailController]
 })
