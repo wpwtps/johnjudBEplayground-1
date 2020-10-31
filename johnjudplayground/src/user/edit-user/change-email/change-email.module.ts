a263cf307fdc70bd8af8f5af2dcf2bb898e039b7
MasterPoon <chirapat.ba@ku.th>
2020-10-31 18:28:16 +0700

    add module

--- johnjudplayground/src/user/edit-user/change-email/change-email.module.ts
+++ johnjudplayground/src/user/edit-user/change-email/change-email.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChangeEmailService } from './change-email.service';
 import { ChangeEmailController } from './change-email.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 
 @Module({
   imports:[
+    TypeOrmModule.forFeature([User]),
     AuthModule,
   ],
   providers: [ChangeEmailService],
