e929de47cf993f9b68655a455b697ad6332ca166
MasterPoon <chirapat.ba@ku.th>
2020-11-23 01:10:08 +0700

    improve word choice

--- johnjudplayground/src/user/edit-user/change-password/change-password.input.ts
+++ johnjudplayground/src/user/edit-user/change-password/change-password.input.ts
@@ -9,7 +9,7 @@ export class ChangePasswordInput{
     @MaxLength(20)
     @Matches(
         /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-        { message: 'password too weak' },
+        { message: 'Password is too weak!' },
     )
     @Field()
     Password: string;
